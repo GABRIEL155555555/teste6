156b49c868dc62aad56ce1e593e43935ccf52025
GABRIEL155555555 <84022090+GABRIEL155555555@users.noreply.github.com>
2021-12-06 15:59:34 -0300

    First commit

---
+++ novoo.php
@@ -0,0 +1,10 @@
+Cluster é um termo em inglês que significa “aglomerar” ou “aglomeração” e pode ser aplicado em vários contextos.
+No caso da computação, o termo define uma arquitetura de sistema capaz combinar vários computadores para trabalharem em conjunto 
+ou pode denominar o grupo em si de computadores combinados.
+
+De forma resumida, o Docker é uma plataforma de código aberto, desenvolvido na linguagem Go e criada pelo próprio Docker. Inc.
+Por ser de alto desempenho, o software garante maior facilidade na criação e administração de ambientes isolados,
+garantindo a rápida disponibilização de programas para o usuário final
+
+POD é o menor conceito do kubernetes,pod pode ter mais de um conteiners,ou pode ser um pod por conteiner.
+
